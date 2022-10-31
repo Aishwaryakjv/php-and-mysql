908d7e833e4d151e405d6836731eae559dc05d77
Aishwaryakjv <81951905+Aishwaryakjv@users.noreply.github.com>
2022-10-31 23:28:52 +0530

    Update header.php

--- departmental_store/template/header.php
+++ departmental_store/template/header.php
@@ -1,4 +1,4 @@
-<head>
+<!-- <head>
 	<title>AtoZ Store</title>
 	<!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -18,6 +18,60 @@
 </head>
 <body class="#ffcdd2 red lighten-4">
 	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text">AtoZ Store</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="add.php" class="btn brand z-depth-0">Add new product</a></li>
+      </ul>
+    </div>
+  </nav> -->
+
+	
+<head>
+	<title>AtoZ Store</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	.brand{
+	  	background: #a1887f !important;
+	  }
+  	.brand-text{
+  		color: #795548  !important;
+  	}
+  	form{
+  		max-width: 460px;
+  		margin: 20px auto;
+  		padding: 20px;
+		
+  	}
+	h4 {
+		color: #af002a ;
+		font-style: revert;
+		font-family: 'Trebuchet MS';
+  		text-decoration-style: solid;
+	}
+	h6 {
+		color: #db7093 ;
+  		text-decoration-style: solid;
+		font-size: 25px;
+	}
+
+	p {
+		color: #db7093;
+       font-size: 25px;
+	   font-family: Arial, Helvetica, sans-serif;
+	}
+	
+   .wrapper{
+	margin: 150px auto;
+  		
+   }
+
+	
+  </style>
+</head>
+<body class="#ffcdd2 red lighten-4">
+	<nav class="white z-depth-100">
     <div class="container">
       <a href="index.php" class="brand-logo brand-text">AtoZ Store</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
