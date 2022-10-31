6aca51e2dd72cc8a171158e2eae86167878e1c8f
Aishwaryakjv <81951905+Aishwaryakjv@users.noreply.github.com>
2022-10-31 21:30:49 +0530

    Create footer.php

---
+++ departmental_store/template/footer.php
@@ -0,0 +1,5 @@
+<footer class="section">
+		<!--<div class="center grey-text">&copy; Copyright 2019 Ninja Pizzas</div>-->
+        <div class="center grey-text">&copy; Copyright 2022 AtoZ Store</div>
+	</footer>
+</body>
