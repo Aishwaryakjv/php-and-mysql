30311a05e4830bea4d322e4f104260b1938964ce
Aishwaryakjv <81951905+Aishwaryakjv@users.noreply.github.com>
2022-10-31 21:30:30 +0530

    Create header.php

---
+++ departmental_store/template/header.php
@@ -0,0 +1,27 @@
+<head>
+	<title>AtoZ Store</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	  .brand{
+	  	background: #a1887f !important;
+	  }
+  	.brand-text{
+  		color: #795548  !important;
+  	}
+  	form{
+  		max-width: 460px;
+  		margin: 20px auto;
+  		padding: 20px;
+  	}
+  </style>
+</head>
+<body class="#ffcdd2 red lighten-4">
+	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="index.php" class="brand-logo brand-text">AtoZ Store</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="add.php" class="btn brand z-depth-0">Add new product</a></li>
+      </ul>
+    </div>
+  </nav>
